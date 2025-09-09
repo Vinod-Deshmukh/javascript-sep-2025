74245911042c83f83575907aeba0f1e2d07daf2d
Vinod-Deshmukh <vinod.256.14@gmail.com>
2025-09-09 23:38:15 +0530

    object

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-// This my first JavaSctipt code.
-// console.log("Hello World");
-let name = "Vinod";
-console.log(name);
+let person = {
+  name: "Vinod",
+  age: 32,
+};
+console.log(person.name);
+person.name = "Deshmukh";
+console.log(person.name);
+console.log(person);
+person["age"] = 30;
+console.log(person.age);
